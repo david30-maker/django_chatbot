d23d52da5abafb0c1bacd273e7f49b4f7acf7ec2
david30-maker <davidokpah396@gmail.com>
2024-10-14 14:57:02 +0100

    modify urls

--- chatbot_project/urls.py
+++ chatbot_project/urls.py
@@ -1,13 +1,3 @@
-''' from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('chatbot/', include('chatbot.urls')),  # This includes the URL patterns from 'chatbot/urls.py'
-] '''
-
-# chatbot_project/urls.py
-
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView
@@ -15,5 +5,5 @@ from django.views.generic import RedirectView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('chatbot/', include('chatbot.urls')),
-    path('', RedirectView.as_view(url='/chatbot/chat/')),  # Redirect root to chatbot
+    path('', RedirectView.as_view(url='/chatbot/chat/')),
 ]
